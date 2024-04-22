c6745d926ea380ca5585cd3d965b3d91879e038e
EvX57 <72947147+EvX57@users.noreply.github.com>
2024-04-22 13:21:07 -0400

    Update scrape_stock.py

--- scrape_stock.py
+++ scrape_stock.py
@@ -2,7 +2,7 @@ import pandas as pd
 import yfinance as yf
 
 ticker = '^GSPC'
-save_path = 'Data/sp500.csv'
+save_path = 'sp500.csv'
 
 snp_prices = yf.download([ticker], start='2002-01-01', end='2024-04-13')
 snp_prices.to_csv(save_path)
